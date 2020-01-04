3ac340a48a74fb499bbde4c419745d9c35314f7a
muryoimpl <muryoimpl@gmail.com>
2020-01-04 23:04:53 +0900

    Add config for multiple monitors in polybar and sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c $HOME/.config/polybar/config myi3 -r &
+# polybar -c $HOME/.config/polybar/config myi3 -r &
+
+if type "xrandr"; then
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar -c $HOME/.config/polybar/config myi3 -r &
+  done
+else
+  polybar -c $HOME/.config/polybar/config myi3 -r &
+fi
 
 echo "Polybar launched..."
