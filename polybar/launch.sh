a60ee1a05f0b12034856fcb39a6faee4dd191242
muryoimpl <muryoimpl@gmail.com>
2020-01-06 00:53:41 +0900

    Show tray only eDP1 display

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,9 +9,14 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 # polybar -c $HOME/.config/polybar/config myi3 -r &
 
+tray_pos=none
+
 if type "xrandr"; then
   for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar -c $HOME/.config/polybar/config myi3 -r &
+    if [[ $m == "eDP1" ]]; then
+      tray_pos=right
+    fi
+    MONITOR=$m TRAY_POSITION=$tray_pos polybar -c $HOME/.config/polybar/config myi3 -r &
   done
 else
   polybar -c $HOME/.config/polybar/config myi3 -r &
