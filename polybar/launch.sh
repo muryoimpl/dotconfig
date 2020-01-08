9f1f55e4529250d5d73e4adb17ab1c823e8b5018
muryoimpl (Ken Muryoi) <k-muryoi@esm.co.jp>
2020-01-08 16:24:31 +0900

    Add config for each display

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,14 +9,29 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 # polybar -c $HOME/.config/polybar/config myi3 -r &
 
-tray_pos=none
-
 if type "xrandr"; then
   for m in $(polybar --list-monitors | cut -d":" -f1); do
     if [[ $m == "eDP1" ]]; then
-      tray_pos=right
+      MONITOR=$m TRAY_POSITION=right polybar -c $HOME/.config/polybar/config myi3 -r &
+      continue
     fi
-    MONITOR=$m TRAY_POSITION=$tray_pos polybar -c $HOME/.config/polybar/config myi3 -r &
+
+    if [[ $m == "HDMI1" ]]; then
+      MONITOR=$m TRAY_POSITION=none polybar -c $HOME/.config/polybar/config myi3hdmi1 -r &
+      continue
+    fi
+
+    if [[ $m == "DP2" ]]; then
+      MONITOR=$m TRAY_POSITION=none polybar -c $HOME/.config/polybar/config myi3dp2 -r &
+      continue
+    fi
+
+    if [[ $m == "HDMI2" ]]; then
+      MONITOR=$m TRAY_POSITION=none polybar -c $HOME/.config/polybar/config myi3hdmi2 -r &
+      continue
+    fi
+
+    MONITOR=$m TRAY_POSITION=none polybar -c $HOME/.config/polybar/config myi3 -r &
   done
 else
   polybar -c $HOME/.config/polybar/config myi3 -r &
