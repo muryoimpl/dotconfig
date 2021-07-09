5ca8ef6845bc307e185785db5e45e920b5d0579a
muryoimpl <muryoimpl@gmail.com>
2021-07-09 14:45:48 +0900

    Update IGNORE_FILES in symlink.rb

--- symlink.rb
+++ symlink.rb
@@ -1,9 +1,8 @@
 require 'fileutils'
 
-IGNORE_FILES = %w(. .. .DS_Store .git README.md LICENSE symlink.rb)
+IGNORE_FILES = %w(. .. .DS_Store .git README.md LICENSE symlink.rb unlink.rb vscode.plugin.txt)
 
 current_dir = Dir.pwd
-
 dotconfigs = Dir.glob('*').reject {|f| IGNORE_FILES.include?(f) }
 
 dotconfigs.each do |dotconf|
