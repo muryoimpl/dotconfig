d386e47c290e5302e4154ef3deb2ebaa68ba6ac3
muryoimpl <ken.muryoi@vivid-garden.co.jp>
2021-12-30 01:03:41 +0900

    Update symlink.rb

--- symlink.rb
+++ symlink.rb
@@ -1,9 +1,18 @@
 require 'fileutils'
+require 'rbconfig'
 
 IGNORE_FILES = %w(. .. .DS_Store .git README.md LICENSE symlink.rb unlink.rb vscode.plugin.txt)
 
+ignored =
+  case RbConfig::CONFIG['host_os']
+  when /darwin/
+    IGNORE_FILES
+  when /linux/
+    IGNORE_FILES + %w(dunst i3 picom polybar rofi systemd)
+  end
+
 current_dir = Dir.pwd
-dotconfigs = Dir.glob('*').reject {|f| IGNORE_FILES.include?(f) }
+dotconfigs = Dir.glob('*').reject {|f| ignored.include?(f) }
 
 dotconfigs.each do |dotconf|
   FileUtils.ln_s("#{current_dir}/#{dotconf}", "#{ENV['HOME']}/.config/#{dotconf}", force: true)
