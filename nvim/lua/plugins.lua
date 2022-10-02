e19b59e1681f839260a89ab33509e9c37ef380de
muryoimpl <muryoimpl@gmail.com>
2022-10-02 23:10:07 +0900

    nvim: treesitterの言語別parserを自動でインストールするようにした

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,7 +16,7 @@ require('jetpack').startup(function (use)
   use { 'maximbaz/lightline-ale' }
   use { 'airblade/vim-gitgutter' }
   use { 'bronson/vim-trailing-whitespace' }
-  use { 'nvim-treesitter/nvim-treesitter' }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use { 'justinmk/vim-dirvish' }
   use { 'Yggdroot/indentLine' }
 
@@ -114,4 +114,20 @@ cmp.setup.cmdline(':', {
   sources = {
     { name = 'cmdline' }
   }
+})
+
+-- nvim-treesitter
+require('nvim-treesitter.configs').setup({
+  sync_install = true,
+  auto_install = true,
+  highlight = {
+    enable= true,
+  },
+  disable = function(lang, buf)
+    local max_filesize = 100 * 1024 -- 100 KB
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    if ok and stats and stats.size > max_filesize then
+      return true
+    end
+  end,
 })
