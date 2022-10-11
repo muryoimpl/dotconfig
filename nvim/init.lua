2a4420b9fa13d2c8080a92dde4e8eafab61bee8c
muryoimpl <muryoimpl@gmail.com>
2022-10-11 23:40:02 +0900

    nvim: aerial.nvimの表示幅と選択時の表示を変更する

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,9 @@ vim.cmd 'colorscheme hybrid'
 -- underline
 vim.opt.cursorline=true
 vim.cmd 'highlight CursorLine gui=underline cterm=underline'
+
+-- aerial
+vim.cmd([[
+  hi AerialLineNC gui=reverse cterm=reverse
+  hi AerialLine   gui=reverse cterm=reverse
+]])
--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -1,4 +1,26 @@
 require('aerial').setup({
+  layout = {
+    -- These control the width of the aerial window.
+    -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    -- min_width and max_width can be a list of mixed types.
+    -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+    max_width = { 50, 0.4 },
+    width = nil,
+    min_width = 10,
+
+    -- Determines the default direction to open the aerial window. The 'prefer'
+    -- options will open the window in the other direction *if* there is a
+    -- different buffer in the way of the preferred direction
+    -- Enum: prefer_right, prefer_left, right, left, float
+    default_direction = "prefer_right",
+
+    -- Determines where the aerial window will be opened
+    --   edge   - open aerial at the far right/left of the editor
+    --   window - open aerial to the right/left of the current window
+    placement = "window",
+  },
+
+
   on_attach = function(bufnr)
     -- Toggle the aerial window with <leader>a
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Space>a', '<cmd>AerialToggle!<CR>', {})
