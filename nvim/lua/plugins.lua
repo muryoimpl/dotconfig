09c391a541aef329317d03814e31282ee4e323cf
muryoimpl <muryoimpl@gmail.com>
2023-02-19 00:21:53 +0900

    nvim: markdown のときはindentLine無効にしてconceallevel=0にする

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -102,6 +102,7 @@ vim.cmd([[
   let g:indentLine_enabled = 1
   let g:indentLine_color_term = 239
   let g:indentLine_char_list = ['|']
+  let g:indentLine_fileTypeExclude = ['markdown']
 ]])
 
 -- lualine
