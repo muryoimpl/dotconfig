dea761c152117f4b3f60667c3048b12aad1b79ec
muryoimpl <muryoimpl@gmail.com>
2023-08-14 22:36:45 +0900

    nvim: LSPの状態をsign列に表示する

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -174,3 +174,10 @@ cmp.setup({
     { name = 'buffer' },
   })
 })
+
+-- Show sign
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
