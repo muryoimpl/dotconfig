b77c77785aab49f42ef67eb33b782904f3f71301
muryoimpl <muryoimpl@gmail.com>
2023-09-02 01:48:33 +0900

    nvim: on_attach.lua 削除

--- nvim/lua/plugins/efm.lua
+++ nvim/lua/plugins/efm.lua
@@ -1,5 +1,4 @@
 -- LSP efm
-local on_attach = require('plugins.on_attach')
 --- linter
 local eslint = require('efmls-configs.linters.eslint')
 local golangci_lint = require('efmls-configs.linters.golangci_lint')
@@ -77,5 +76,34 @@ local efmls_config = {
     documentRangeFormatting = true,
   },
 }
+
+local lspconfig = require('lspconfig')
+local util = require('lspconfig/util')
+---@diagnostic disable:undefined-global
+-- https://zenn.dev/botamotch/articles/21073d78bc68bf
+-- nvim-lspconfig のキーバインドを設定する
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+  -- print(client.name)
+  local lspopts = { noremap = true, silent = true, buffer = ev.buf }
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', lspopts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', lspopts)
+  vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>', lspopts)
+  vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.format()<CR>', lspopts)
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', lspopts)
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', lspopts)
+  vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', lspopts)
+  vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', lspopts)
+  vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', lspopts)
+  vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', lspopts)
+  vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>', lspopts)
+  vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', lspopts)
+  -- vim.keymap.set('n', '<space>o', '<cmd>lua vim.diagnostic.setqflist()<CR>', lspopts)
+  vim.keymap.set('n', '<space>t', function()
+    vim.lsp.buf.format { async = true }
+  end, lspopts)
+end
 
 require('lspconfig').efm.setup(vim.tbl_extend('force', efmls_config, { on_attach = on_attach}))
