e3858101c21ed38ef8c38c370422ae35cba5ade8
muryoimpl <muryoimpl@gmail.com>
2023-10-11 09:05:39 +0900

    nvim: copilotのsuggestionを nvim-cmp に出すようにする
    nvim-cmp の highlight を github-theme の all
    の中に設定しないと効かなかったので、そちらに含めている

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -13,11 +13,30 @@ vim.opt.completeopt = 'menu,menuone,noselect'
 local cmp = require('cmp')
 cmp.setup({
   formatting = {
-    format = require('lspkind').cmp_format({
-      mode = "symbol",
-      max_width = 50,
-      symbol_map = { Copilot = "" },
-    }),
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      local lspkind = require('lspkind')
+
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        vsnip = "[VSnip]",
+      })[entry.source.name]
+
+      if vim.tbl_contains({ 'path' }, entry.source.name) then
+        local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+        if icon then
+          vim_item.kind = icon
+          vim_item.kind_hl_group = hl_group
+          return vim_item
+        end
+      end
+
+      return lspkind.cmp_format({
+        mode = "symbol",
+        symbol_map = { Copilot = "" },
+      })(entry, vim_item)
+    end
   },
   snippet = {
     expand = function(args)
@@ -33,9 +52,33 @@ cmp.setup({
     ['<C-y>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = nil,
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      else
+        fallback()
+      end
+    end),
     ['<S-Tab>'] = nil,
   }),
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      require("copilot_cmp.comparators").prioritize,
+
+      -- Below is the default comparitor list and order for nvim-cmp
+      cmp.config.compare.offset,
+      -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "vsnip" },
