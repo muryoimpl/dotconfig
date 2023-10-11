c8e0e981bb5d85f2974649a6cc3a510ed77cfed3
muryoimpl <muryoimpl@gmail.com>
2023-10-12 00:10:24 +0900

    nvim: nvim-cmpの補完menuの色設定を追加する
    bg 設定されるとわかりにくいものがあったので、いくつか bg を取り除いた
    https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-dark-theme-colors-to-the-menu

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -26,42 +26,42 @@ require("github-theme").setup({
       Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
 
       CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", strikethrough = true },
-      CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE", bold = true },
+      CmpItemAbbrMatch = { fg = "#82AAFF",      bg = "NONE", bold = true },
       CmpItemAbbrMatchFuzzy = { fg = "#82AAFF", bg = "NONE", bold = true },
-      CmpItemMenu = { fg = "#C792EA", bg = "NONE", italic = true },
+      CmpItemMenu = { fg = "#C792EA",           bg = "NONE", italic = true },
 
-      CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
-      CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
-      CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+      CmpItemKindField = { fg = "#EED8DA",       },  -- bg = "#B5585F" },
+      CmpItemKindProperty = { fg = "#EED8DA",    },  -- bg = "#B5585F" },
+      CmpItemKindEvent = { fg = "#EED8DA",       },  -- bg = "#B5585F" },
 
-      CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
-      CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
-      CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+      CmpItemKindText = { fg = "#C3E88D",        },   -- bg = "#9FBD73" },
+      CmpItemKindEnum = { fg = "#C3E88D",        },   -- bg = "#9FBD73" },
+      CmpItemKindKeyword = { fg = "#C3E88D",     },   -- bg = "#9FBD73" },
 
-      CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
-      CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
-      CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+      CmpItemKindConstant = { fg = "#FFE082",    },   -- bg = "#D4BB6C" },
+      CmpItemKindConstructor = { fg = "#FFE082", },   -- bg = "#D4BB6C" },
+      CmpItemKindReference = { fg = "#FFE082",   },   -- bg = "#D4BB6C" },
 
       CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
-      CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
-      CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
-      CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+      CmpItemKindStruct = { fg = "#EADFF0",   bg = "#A377BF" },
+      CmpItemKindClass = { fg = "#EADFF0",    bg = "#A377BF" },
+      CmpItemKindModule = { fg = "#EADFF0",   bg = "#A377BF" },
       CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
 
       CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
-      CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+      CmpItemKindFile = { fg = "#C5CDD9",     bg = "#7E8294" },
 
       CmpItemKindUnit = { fg = "#F5EBD9", },
       CmpItemKindSnippet = { fg = "#F5EBD9",  },
       CmpItemKindFolder = { fg = "#F5EBD9",  },
 
-      CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
-      CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+      CmpItemKindMethod = { fg = "#DDE5F5",     bg = "#6C8ED4" },
+      CmpItemKindValue = { fg = "#DDE5F5",      bg = "#6C8ED4" },
       CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
 
-      CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
-      CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
-      CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+      CmpItemKindInterface = { fg = "#D8EEEB",      bg = "#58B5A8" },
+      CmpItemKindColor = { fg = "#D8EEEB",          bg = "#58B5A8" },
+      CmpItemKindTypeParameter = { fg = "#D8EEEB",  bg = "#58B5A8" },
 
       CmpItemKindCopilot = { fg ="#6CC644" },
       -- nvim-cmp の配色 end
