70080d958b9680318cbc0090a2af5297d11e7a7b
muryoimpl <muryoimpl@gmail.com>
2024-03-20 23:16:46 +0900

    nvim: indentの事故が多いためtreesitterのindent設定を無効にする

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -216,7 +216,7 @@ require("lazy").setup({
       require('nvim-treesitter.configs').setup({
         sync_install = true,
         auto_install = true,
---        indent = { enable = true },
+        -- indent = { enable = true },
         highlight = {
           enable = true,
           disable = function(lang, buf)
