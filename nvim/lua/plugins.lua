25b3b25aa4b2ba19bbc29ba6bd7af8a1b2b2c4a8
muryoimpl <muryoimpl@gmail.com>
2024-05-26 09:13:37 +0900

    nvim: nvim-treesitterの設定を変更する/playground設定の除去

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -256,15 +256,12 @@ require("lazy").setup({
   },
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/playground'
-    },
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
         sync_install = true,
         auto_install = true,
-        -- indent = { enable = true },
+        indent = { enable = true },
         highlight = {
           enable = true,
           disable = function(lang, buf)
@@ -283,31 +280,28 @@ require("lazy").setup({
         },
         incremental_selection = { enable = true },
         textobjects = { enable = true },
-
-       -- playground
-       playground = {
-         enable = true,
-         disable = {},
-         updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-         persist_queries = false, -- Whether the query persists across vim sessions
-         keybindings = {
-           toggle_query_editor = 'o',
-           toggle_hl_groups = 'i',
-           toggle_injected_languages = 't',
-           toggle_anonymous_nodes = 'a',
-           toggle_language_display = 'I',
-           focus_language = 'f',
-           unfocus_language = 'F',
-           update = 'R',
-           goto_node = '<cr>',
-           show_help = '?',
-         },
-       },
-       query_linter = {
-         enable = true,
-         use_virtual_text = true,
-         lint_events = { "BufWrite", "CursorHold" },
-       },
+        autotag = { enable = ture },
+        ensure_installed = {
+          "bash",
+          "c",
+          "cpp",
+          "css",
+          "dockerfile",
+          "go",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "python",
+          "ruby",
+          "rust",
+          "typescript",
+          "yaml",
+          "sql",
+          "markdown",
+          "markdown_inline",
+          "typescript",
+        },
       })
     end,
   },
