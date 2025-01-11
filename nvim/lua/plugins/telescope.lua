9659dbfd43bfc752a8a5ed536790fb9afbafabca
muryoimpl <muryoimpl@gmail.com>
2025-01-11 14:20:30 +0900

    nvim: <space>tr で treesitter を呼び出すよう変更
    <space>ts が nvim-test の TestSuite を被っていたため、こちらを変更する

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -186,7 +186,7 @@ vim.keymap.set('n', '<space>qf', function() bltin.quickfix()                  en
 vim.keymap.set('n', '<space>lc', function() bltin.loclist()                   end, kopts)
 vim.keymap.set('n', '<space>au', function() bltin.autocommands()              end, kopts)
 vim.keymap.set('n', '<space>hp', function() bltin.help_tags()                 end, kopts)
-vim.keymap.set('n', '<space>ts', function() bltin.treesitter()                end, kopts)
+vim.keymap.set('n', '<space>tr', function() bltin.treesitter()                end, kopts)
 vim.keymap.set('n', '<space>ma', function() bltin.man_pages()                 end, kopts)
 
 local chat_actions = require("CopilotChat.actions")
