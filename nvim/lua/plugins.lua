392ba71077c50ba556dc0db37277c77f61487165
muryoimpl <muryoimpl@gmail.com>
2025-02-08 13:29:00 +0900

    nvim: Remove telescope-fzf-native.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -564,8 +564,7 @@ require("lazy").setup({
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8', branch = '0.1.x',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-lua/plenary.nvim"
     },
   },
 
