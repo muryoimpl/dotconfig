f4e7521941628c50d993d55ffd16c5cd761032e7
muryoimpl <muryoimpl@gmail.com>
2025-10-20 00:42:58 +0900

    mangowc: wip config

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -1,1 +1,29 @@
+#! /bin/bash
 
+set +e
+
+waybar -c ~/.config/waybar/config.mango.jsonc -s ~/.config/waybar/style.css &
+
+# message
+swaymsg layout default &
+
+# notification
+swaync &
+
+# keep clipboard content
+wl-clip-persist --clipboard regular --reconnect-tries 0 &
+
+# clipboard content manager
+wl-paste --type text --watch cliphist store &
+
+# network manager
+nm-applet --indicator &
+
+# IME
+fcitx5 -d
+
+# password manager
+1password --silent &
+
+# bluetooth
+blueman-applet
