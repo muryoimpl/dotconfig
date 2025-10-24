a026b8facef211e81f43ec0116ce889753561adb
muryoimpl <muryoimpl@gmail.com>
2025-10-25 02:25:15 +0900

    mangowc: autostart.sh に起動アプリを追加

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -1,32 +1,54 @@
 #! /bin/bash
 
+# need to install
+# - waybar
+# - swaybg
+# - swaymsg
+# - swaync
+# - swayosd
+# - wl-clip-persist
+# - wl-paste
+# - network-manager
+# - fcitx5
+# - 1password
+# - blueman
+# - xfce-polkit
+
 set +e
 
-waybar -c ~/.config/waybar/config.mango.jsonc -s ~/.config/waybar/style.css &
+swaybg -i ~/.config/mango/Ju5PuBC-arch-linux-wallpaper.jpg -m fill >/dev/null 2>&1 &
+
+waybar -c ~/.config/waybar/config.mango.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
 
 # message
-swaymsg layout default &
+swaymsg layout default >/dev/null 2>&1 &
 
 # notification
-swaync &
+swaync >/dev/null 2>&1 &
 
 # keep clipboard content
-wl-clip-persist --clipboard regular --reconnect-tries 0 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 
 # clipboard content manager
-wl-paste --type text --watch cliphist store &
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
 
 # network manager
-nm-applet --indicator &
+nm-applet --indicator >/dev/null 2>&1 &
 
 # IME
-fcitx5 -d
+fcitx5 -d >/dev/null 2>&1 &
 
 # password manager
-1password --silent &
+1password --silent >/dev/null 2>&1 &
 
 # bluetooth
-blueman-applet &
+blueman-applet >/dev/null 2>&1 &
 
 # screen layout
-~/.screenlayout/default.sh &
+~/.screenlayout/default.sh >/dev/null 2>&1 &
+
+# Permission authentication
+/usr/lib/xfce-polkit/xfce-polkit >/dev/null 2>&1 &
+
+# change light and volume value by swayosd-client
+swayosd-server >/dev/null 2>&1 &
