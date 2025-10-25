66b1fd45c507295b1e37dacbe5e67e8c09618f34
muryoimpl <muryoimpl@gmail.com>
2025-10-26 02:21:32 +0900

    rofi: walker をやめて rofi に戻る
    google-chrome-stable が動作しなかったんや…

--- rofi/rofi_cmd.sh
+++ rofi/rofi_cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rofi -modi combi,"clipboard:greenclip print",run,ssh,drun,"emoji:rofimoji" -show combi -combi-modi window,run,clipboard,ssh,drun,keys,emoji
+rofi -modi combi,"clipboard:greenclip print",run,ssh,drun,emoji -show combi -combi-modi window,run,clipboard,ssh,drun,keys,emoji
