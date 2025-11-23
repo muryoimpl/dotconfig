bdcd12c59f528dfa7028fe43577001b84c5629da
muryoimpl <muryoimpl@gmail.com>
2025-11-23 14:53:04 +0900

    symlink.rb: 相対パスで処理できるように見直し

--- symlink.rb
+++ symlink.rb
@@ -2,6 +2,7 @@ require 'fileutils'
 require 'rbconfig'
 
 IGNORE_FILES = %w(. .. .DS_Store .git README.md LICENSE symlink.rb unlink.rb vscode.plugin.txt)
+RELATIVE_PATH = './ghq/github.com/muryoimpl/dotconfig'
 
 ignored =
   case RbConfig::CONFIG['host_os']
@@ -14,11 +15,14 @@ ignored =
 current_dir = Dir.pwd
 dotconfigs = Dir.glob('*').reject {|f| ignored.include?(f) }
 
+FileUtils.cd "#{ENV['HOME']}/.config/"
+
 dotconfigs.each do |dotconf|
-  file_in_home = "#{ENV['HOME']}/.config/#{dotconf}"
+  file_in_home = "./#{dotconf}"
   FileUtils.safe_unlink(file_in_home) if FileTest.symlink?(file_in_home)
-  FileUtils.ln_s("#{current_dir}/#{dotconf}", "#{ENV['HOME']}/.config/#{dotconf}", force: true)
-  puts "#{current_dir}/#{dotconf}  ->  #{ENV['HOME']}/.config/#{dotconf}"
+
+  FileUtils.ln_s("../#{RELATIVE_PATH}/#{dotconf}", "./#{dotconf}", force: true)
+  puts "$HOME/#{RELATIVE_PATH}/#{dotconf}  ->  $HOME/.config/#{dotconf}"
 rescue
   puts "Error occurred. #{dotconf}, file_in_home: #{file_in_home}"
 end
