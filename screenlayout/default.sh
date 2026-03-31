98c9896e9ff282d8d7d6b8a3c9bc03225dd56aa6
muryoimpl <muryoimpl@gmail.com>
2026-03-31 11:28:24 +0900

    screenlayout: 仕事用のディスプレイ設定を default.sh 内で分岐するように変更

--- screenlayout/default.sh
+++ screenlayout/default.sh
@@ -1,3 +1,17 @@
-wlr-randr --output HDMI-A-1 --preferred --pos 0,400 \
-          --output DP-1 --preferred --pos 3840,400 \
-          --output DP-3 --preferred --pos 7680,0 --transform 90
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+
+if [[ "$HOSTNAME" == *"work"* ]]; then
+  wlr-randr --output HDMI-A-1 --preferred --pos 7680,0 --transform 90 \
+            --output DP-1 --preferred --pos 3840,400 \
+            --output DP-2 --preferred --pos 0,400
+
+  echo 'screenlayout: work'
+else
+  wlr-randr --output HDMI-A-1 --preferred --pos 0,400 \
+            --output DP-1 --preferred --pos 3840,400 \
+            --output DP-3 --preferred --pos 7680,0 --transform 90
+
+  echo 'screenlayout: default'
+fi
--- screenlayout/work.sh
+++
@@ -1,3 +0,0 @@
-wlr-randr --output HDMI-A-1 --preferred --pos 6400,0 --transform 90 \
-          --output DP-1 --preferred --pos 2560,400 \
-          --output DP-2 --preferred --pos 0,1120
